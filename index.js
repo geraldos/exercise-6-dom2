c09125312cab6593c3e1ef58ddbec09fc6b8a184
geraldos <sepdwijaya18@gmail.com>
2020-06-16 15:27:40 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let h1 = document.querySelector("#text")
+
+text.style.color = 'blue';
+text.style.fontSize = 'xx-large'
+text.style.fontFamily = 'Impact,Charcoal,sans-serif'
+
+let section = document.querySelector(".background")
+console.log(section)
+section.style.background = 'red';
